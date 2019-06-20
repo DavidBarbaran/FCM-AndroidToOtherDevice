2ec84d9c1355706adf47c1061e19c384e8d810c8
DavidBarbaran <alejandro.david.barbaran@gmail.com>
2019-06-20 11:51:11 -0500

    Migrate project to Android X

--- app/src/main/java/barbaran/david/fcmandroidtoandroiddemo/MainActivity.kt
+++ app/src/main/java/barbaran/david/fcmandroidtoandroiddemo/MainActivity.kt
@@ -1,6 +1,6 @@
 package barbaran.david.fcmandroidtoandroiddemo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import fcm.androidtoandroid.FirebasePush
